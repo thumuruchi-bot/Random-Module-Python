924b044d5ae9a7020def9532f17ff0416c68119e
T.Ruchitha <thumuruchi@gmail.com>
2026-09-20 11:07:31 +0530

    Create random_module.py

---
+++ random_module.py
@@ -0,0 +1,9 @@
+import random
+
+number = random.randint(1, 10)
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+fruit = random.choice(fruits)
+
+print("Random Number:", number)
+print("Random Fruit:", fruit)
